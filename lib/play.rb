155fd4caf2a3900983aa1636694017e2a7ca2e8b
Sherief Farid <sherief@farid.cloud>
2019-02-13 02:29:31 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -37,15 +37,10 @@ end
 
 # Define your play method below
 def play(board)
-
+  counter = 0
+  while counter <8
     turn(board)
-  turn(board)
-    turn(board)
-      turn(board)
-        turn(board)
-          turn(board)
-            turn(board)
-              turn(board)
-                turn(board)
-
+    counter +=1
+    
+  end
 end
